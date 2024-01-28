edf18c667c21260d008a715b70aae71768f5f69a
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2024-01-28 16:03:55 +0100

    Use CRT to achieve faster solution

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -14,9 +14,41 @@ fn lcm(x: usize, y: usize) -> usize {
     x * y / gcd(x, y)
 }
 
+#[derive(Debug)]
+struct EucledianResult {
+    s: i64,
+    t: i64,
+    coefficients: (i64, i64),
+    gcd: i64
+}
+
+fn extended_euclidian(a: i64, b: i64) -> EucledianResult {
+    let mut s = 0;
+    let mut t = 1;
+    let mut r = b;
+    let mut old_s = 1;
+    let mut old_t = 0;
+    let mut old_r = a;
+    while r != 0 {
+        let quotient = old_r / r;
+        (old_r, r) = (r, old_r - quotient * r); // r = old_r % r
+        (old_s, s) = (s, old_s - quotient * s); // s = old_s % s
+        (old_t, t) = (t, old_t - quotient * t); // t = old_t % t
+    }
+    return EucledianResult { s,
+                             t,
+                             coefficients: (old_s, old_t),
+                             gcd: old_r }
+}
+
+fn inverse_mod(x: i64, n: i64) -> i64 {
+    let eea = extended_euclidian(x, n);
+    (eea.coefficients.0 + n) % n
+}
+
 struct Disc {
-    len: usize,
-    startpos: usize
+    len: u32,
+    startpos: u32
 }
 
 impl From<&str> for Disc {
@@ -31,22 +63,40 @@ impl From<&str> for Disc {
 
 fn fallthrough(discs: &[Disc]) -> Option<usize> {
     for i in (0..) {
-        if discs.iter().enumerate().all(|(t, d)| (d.startpos + i + t + 1) % d.len == 0) {
-           return Some(i);
+        if discs.iter().enumerate().all(|(t, d)| (d.startpos + i + t as u32 + 1) % d.len == 0) {
+           return Some(i as usize);
         }
     }
     None
 }
 
+fn fallthrough_chinese_remainder_theorem(discs: &[Disc]) -> Option<usize> {
+    // TODO: fix this nightmare of casts and iterators
+    let N = discs.iter().map(|d| d.len).product::<u32>();
+    let N_i = discs.iter().map(|d| N / d.len).collect::<Vec<u32>>();
+    let inverses: Vec<_> = N_i.iter().zip(discs.iter())
+                              .map(|(n, d)| inverse_mod((*n).into(), d.len.into()))
+                              .collect();
+    let startvals = discs.iter().enumerate().map(|(t, d)| d.len as i32 - d.startpos as i32 - t as i32 - 1).collect::<Vec<_>>();
+    let mut result = 0;
+    for i in (0..N_i.len()) {
+        result += N_i[i] as i64 * inverses[i] as i64 * startvals[i] as i64;
+    }
+    if result < 0 {
+        result = (result % N as i64) + N as i64;
+    }
+    Some(result as usize % N as usize)
+}
+
 fn part1(lines: &Vec<&str>) -> Option<usize> {
     let discs = lines.iter().map(|l| Disc::from(*l)).collect::<Vec<_>>();
-    fallthrough(&discs)
+    fallthrough_chinese_remainder_theorem(&discs)
 }
 
 fn part2(lines: &Vec<&str>) -> Option<usize> {
     let mut discs = lines.iter().map(|l| Disc::from(*l)).collect::<Vec<_>>();
     discs.push(Disc{len: 11, startpos: 0});
-    fallthrough(&discs)
+    fallthrough_chinese_remainder_theorem(&discs)
 }
 
 fn main() {
@@ -85,5 +135,33 @@ mod tests {
     fn test_part1() {
         let discs = [Disc{len: 5, startpos: 4}, Disc{len: 2, startpos: 1}];
         assert_eq!(Some(5), fallthrough(&discs))
+    }
+
+    #[test]
+    fn test_part1_cmt() {
+        let discs = [Disc{len: 5, startpos: 4}, Disc{len: 2, startpos: 1}];
+        assert_eq!(Some(5), fallthrough_chinese_remainder_theorem(&discs))
+    }
+
+    #[test]
+    fn test_eea() {
+        let eea_result = extended_euclidian(6, 12);
+        assert_eq!(6, eea_result.gcd);
+    }
+
+    #[test]
+    fn test_inverse() {
+        let inverse = inverse_mod(3, 5);
+        assert_eq!(2, inverse);
+        let inverse = inverse_mod(inverse, 5);
+        assert_eq!(3, inverse);
+    }
+
+    #[test]
+    fn test_inverse_2() {
+        let inverse = inverse_mod(11, 26);
+        assert_eq!(19, inverse);
+        let inverse = inverse_mod(inverse, 26);
+        assert_eq!(11, inverse);
     }
 }
