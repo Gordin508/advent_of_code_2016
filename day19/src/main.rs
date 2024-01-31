f1cd5dd9e72cad782bf79054ce8a87625982d976
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2024-01-31 20:25:31 +0100

    Solve day19 part2

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -19,9 +19,57 @@ fn last_elf_standing(num_elves: usize) -> usize {
     winning_elf + 1
 }
 
-fn last_elf_standing_pt2(num_elves: usize) -> usize {
+fn last_elf_standing_pt2_brute_force(num_elves: usize) -> usize {
     // implement me!
-    0
+    let mut elves = vec![1u8; num_elves];
+    let mut num_elves = num_elves;
+    let mut idx: usize = 0;
+    while num_elves > 1 {
+        let skip = num_elves / 2;
+        let mut skipped: usize = 0;
+        let mut index: usize = idx;
+        loop {
+            skipped += elves[index] as usize;
+            if skipped > skip {
+                break;
+            }
+            index = (index + 1) % elves.len();
+        }
+        elves[index] = 0;
+        num_elves -= 1;
+        idx = (idx + 1) % elves.len();
+        while (elves[idx] == 0) {
+            idx = (idx + 1) % elves.len();
+        }
+    }
+    elves.into_iter().enumerate().filter(|(i, elf)| *elf == 1).next().unwrap().0 + 1
+}
+
+fn last_elf_standing_pt2(num_elves: usize) -> usize {
+    if num_elves < 6 {
+        return last_elf_standing_pt2_brute_force(num_elves);
+    }
+    let mut root = 1;
+    let mut three_pow = 3;
+    loop {
+        let newpow = three_pow * 3;
+        if newpow > num_elves {
+            break;
+        }
+        three_pow = newpow;
+        root += 2;
+    }
+    // you can find this pattern by bruteforcing the first 100 solutions
+    // and manually inspecting them
+    if num_elves == three_pow {
+        return num_elves;
+    }
+    if (num_elves - three_pow) < three_pow {
+        return num_elves - three_pow;
+    } else {
+        let next_three_pow = 3 * three_pow;
+        return (num_elves - (next_three_pow - num_elves));
+    }
 }
 
 fn part1(lines: &Vec<&str>) -> Option<usize> {
@@ -31,8 +79,9 @@ fn part1(lines: &Vec<&str>) -> Option<usize> {
 }
 
 fn part2(lines: &Vec<&str>) -> Option<usize> {
-    //TODO: implement me
-    None
+    assert_eq!(1, lines.len());
+    let puzzle_input = lines[0].parse::<usize>().unwrap();
+    Some(last_elf_standing_pt2(puzzle_input))
 }
 
 fn main() {
@@ -88,5 +137,8 @@ mod tests {
     #[test]
     fn test_last_elf_pt2() {
         assert_eq!(2, last_elf_standing_pt2(5));
+        assert_eq!(5, last_elf_standing_pt2(7));
+        assert_eq!(1, last_elf_standing_pt2_brute_force(10));
+        assert_eq!(1, last_elf_standing_pt2(10));
     }
 }
