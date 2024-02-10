f4e884b88d228f58a5653874b0fa4ed7fac65b85
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2024-02-10 16:10:07 +0100

    Slow but working solution

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -30,6 +30,21 @@ struct NodeGrid {
     height: usize
 }
 
+use std::fmt::Display;
+
+impl Display for NodeGrid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let node = &self[(x, y)];
+                write!(f, "({}/{}) ", node.used, node.size)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
 impl NodeGrid {
     fn new(nodes: &[Node], width: usize, height: usize) -> Self {
         assert_eq!(nodes.len(), width * height);
@@ -89,13 +104,18 @@ fn part1(lines: &Vec<&str>) -> Option<usize> {
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
 struct QueueEntry {
-    grid: NodeGrid,
-    datapos: (usize, usize)
+    emptypos: (usize, usize),
+    datapos: (usize, usize),
 }
 
 impl QueueEntry {
-    fn new(grid: NodeGrid, datapos: (usize, usize)) -> Self {
-        QueueEntry { grid, datapos }
+    fn from_grid(grid: &NodeGrid, datapos: (usize, usize)) -> Self {
+        let emptypos = grid.nodes.iter().filter(|n| n.used == 0).map(|n| (n.x, n.y)).next().unwrap();
+        QueueEntry { emptypos, datapos }
+    }
+
+    fn new(grid: NodeGrid, emptypos: (usize, usize), datapos: (usize, usize)) -> Self {
+        QueueEntry { emptypos, datapos }
     }
 }
 
@@ -104,32 +124,44 @@ fn part2(lines: &Vec<&str>) -> Option<usize> {
     let nodes = parse_input(lines);
     assert_eq!(lines.len() - 2, nodes.len());
     let (width, height) = nodes.iter().map(|n| (n.x + 1, n.y + 1)).max().unwrap();
-    println!("Nodes: {}, width: {}, height: {}", nodes.len(), width, height);
     let grid = NodeGrid::new(&nodes, width as usize, height as usize);
-    assert_eq!(grid[(1, 4)].x, 1);
+    assert_eq!(grid[(1, 4)].x, 1); // sanity check
     assert_eq!(grid[(1, 4)].y, 4);
     let mut step = 0;
-    let mut queue = vec![QueueEntry::new(grid, (width, 0))];
+    let mut queue = vec![QueueEntry::from_grid(&grid, (width - 1, 0))];
     let mut seen = HashSet::new();
     seen.insert(queue[0].clone());
+
+    let neighbors = |grid: &NodeGrid, (x, y)| -> Vec<Node> {
+        let mut result = Vec::new();
+        if x > 0 {
+            result.push(grid[(x - 1, y)].clone());
+        }
+        if x < grid.width - 1 {
+            result.push(grid[(x + 1, y)].clone());
+        }
+        if y > 0 {
+            result.push(grid[(x, y - 1)].clone());
+        }
+        if y < grid.height - 1 {
+            result.push(grid[(x, y + 1)].clone());
+        }
+        result
+    };
     while queue.len() != 0 {
         let mut newqueue = Vec::new();
         for entry in queue {
             if entry.datapos == (0, 0) {
                 return Some(step)
             }
-            for node1 in entry.grid.nodes.iter() {
-                for node2 in entry.grid.nodes.iter() {
-                    if nodes_viable(node1, node2, true) {
-                        // new queueentry with updated notes
-                        let mut newgrid = entry.grid.clone();
-                        newgrid[(node2.x, node2.y)].used += newgrid[(node1.x, node1.y)].used;
-                        newgrid[(node1.x, node1.y)].used = 0;
-                        let datapos = if (node1.x, node1.y) == entry.datapos {(node2.x, node2.y)} else {entry.datapos};
-                        let new_entry = QueueEntry::new(newgrid, datapos);
-                        if seen.insert(new_entry.clone()) {
-                            newqueue.push(new_entry);
-                        }
+            // for all neighbors in emptypos
+            let emptypos = entry.emptypos;
+            for n in neighbors(&grid, emptypos) {
+                if n.used <= grid[emptypos].size {
+                    let datapos = if (n.x, n.y) == entry.datapos {emptypos} else {entry.datapos};
+                    let newentry = QueueEntry::new(grid.clone(), (n.x, n.y), datapos);
+                    if seen.insert(newentry.clone()) {
+                        newqueue.push(newentry);
                     }
                 }
             }
